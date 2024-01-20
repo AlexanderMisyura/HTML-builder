f40cd6f1f26485e2805c8c4a314fcccd9dd84e13
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2024-01-20 18:12:39 +0300

    feat: add files-in-folder solution to HTML-builder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,22 @@
+const { readdir, stat } = require('fs/promises');
+const path = require('path');
+
+const dirPath = path.join(__dirname, './secret-folder');
+
+async function readFiles() {
+  const files = await readdir(dirPath);
+
+  for (const fileName of files) {
+    const filePath = path.join(dirPath, fileName);
+    const fileStat = await stat(filePath);
+
+    if (fileStat.isFile()) {
+      const ext = path.extname(fileName);
+      const name = path.basename(fileName, ext);
+      const size = fileStat.size;
+      console.log(`${name} - ${ext.slice(1)} - ${size}`);
+    }
+  }
+}
+
+readFiles();
